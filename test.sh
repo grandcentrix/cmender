043e1ef954b203a817376089be82b697dbe9190e
Employee One <employee-one@grandcentrix.net>
2019-05-20 10:50:49 +0200

    Only link to jsmn if its libraryfile is really found.
    As this header only library generates each function once each time it has been included, it must be included in the test.sh script as system include to disable -Werror=unused-function errors.

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ build() {
 
     git clone https://github.com/zserge/jsmn.git
     make -C jsmn
-    export CFLAGS="$CFLAGS -I$(pwd)/jsmn"
+    export CFLAGS="$CFLAGS -isystem $(pwd)/jsmn"
     export LDFLAGS="$LDFLAGS -L$(pwd)/jsmn"
 
     cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTING=ON -DCODE_COVERAGE=ON ..
